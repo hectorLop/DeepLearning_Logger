04817c7c25cb1d8cbbdec9bd61a2b0972c6a5f62
hectorLop <lopez.almazan.hector@gmail.com>
2021-02-13 23:44:24 +0100

    Keras_logger: Refactored the method to create folders and added some keyword arguments

--- src/keras/project.py
+++ src/keras/project.py
@@ -8,13 +8,15 @@ class Project():
     def __init__(self, project_name: str, project_path: str=os.path.dirname(os.path.realpath(__file__))) -> None:
         self._project_path = project_path
         self._project_name = project_name
-        self._project_folder_path = self._create_project_folder()
+        self._project_folder_path = self._create_folder([project_path, project_name])
 
-    def create_experiment(self, experiment_name: str, configs: List[Config]):
+    def create_experiment(self, experiment_name: str='', configs: List[Config]=[]):
         if not experiment_name:
             raise ValueError('Must use a non empty experiment name')
+        if not configs:
+            raise ValueError('The configurations list is empty, there are nothing to log')
 
-        experiment_folder_path = self._create_experiment_folder(experiment_name)
+        experiment_folder_path = self._create_folder([self._project_folder_path, experiment_name])
         experiment = Experiment(experiment_folder_path, experiment_name, configs=configs)
         experiment.register_experiment()
 
@@ -24,19 +26,21 @@ class Project():
     def list_experiments(self):
         pass
 
-    def _create_experiment_folder(self, experiment_name: str) -> str:
-        experiment_folder_path = self._project_folder_path  + experiment_name + '/'
+    def _create_folder(self, paths: List[str]=[]):
+        if not paths:
+            raise ValueError('Path lists cannot be empty')
+        
+        folder_path = ''
 
-        os.makedirs(experiment_folder_path)
+        for path in paths:
+            if path[-1] == '/':
+                folder_path += path
+            else:
+                folder_path += path + '/'
 
-        return experiment_folder_path
+        if not os.path.isdir(folder_path):
+            os.makedirs(folder_path)
 
-    def _create_project_folder(self):
-        project_folder_path = self._project_path + '/' + self._project_name + '/'
-
-        if not os.path.isdir(project_folder_path):
-            os.makedirs(project_folder_path)
-
-        return project_folder_path
+        return folder_path
 
         
