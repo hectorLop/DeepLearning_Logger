c458745fe0401a41c794f5c3552ffc0fc888fdd7
hectorLop <lopez.almazan.hector@gmail.com>
2021-03-13 20:40:56 +0100

    Keras_Configs: Refactored to allow the creation a Config object from a configuration dictionary

--- deeplearning_logger/keras/configs.py
+++ deeplearning_logger/keras/configs.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Callable, Any, Dict, Tuple
 import numpy as np
 from typeguard import typechecked
 from tensorflow.keras.models import Model
@@ -7,51 +7,84 @@ from tensorflow.keras.callbacks import Callback
 import pandas as pd
 
 class Config(ABC):
-    def __init__(self, data: object) -> None:
-        self._data = data
+    """
+    Configuration base class of objects that the Experiment can log.
+
+    Parameters
+    ----------
+    data : Any
+        Data to be stored in an experiment.
+
+    Attributes
+    ----------
+    _data : Any
+        Data to be stored in an experiment.
+    """
+    def __init__(self, data: Any) -> None:
+        if isinstance(data, Dict): # Creation from a config dictionary
+            self._config = data
+        else:
+            self._config = self.get_config(data)
 
     @abstractmethod
-    def get_config(self):
+    def get_config(self, data) -> Tuple[str, Dict]:
         pass
 
+    @property
+    def config(self) -> Tuple:
+        return self._config
+
 class MetricsConfig(Config):
+    """
+    Config subclass to store the model training metrics
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        DataFrame containing the model metrics
+
+    Attributes
+    ----------
+    _data : pd.DataFrame
+        DataFrame containing the model metrics
+    """
     @typechecked
     def __init__(self, data: pd.DataFrame) -> None:
         super().__init__(data)
 
-    def get_config(self):
+    def get_config(self, data):
         config = {}
 
-        for idx, row in self._data.iterrows():
+        for idx, row in data.iterrows():
             config[f'epoch_{idx}'] = {}
-            for column in self._data.columns:
+            for column in data.columns:
                 config[f'epoch_{idx}'][column] = float(row.loc[column])
         
-        return 'metrics', config
+        return ('metrics', config)
 
 class ModelConfig(Config):
     @typechecked
     def __init__(self, model: Model) -> None:
         super().__init__(model)
 
-    def get_config(self):
-        model_architecture = self._data.get_config()
-        optimizer_config = self._data.optimizer.get_config()
+    def get_config(self, data):
+        model_architecture = data.get_config()
+        optimizer_config = data.optimizer.get_config()
 
         config = {
             'model_config': model_architecture,
             'optimizer_config': optimizer_config
         }
 
-        return 'model', config
+        return ('model', config)
 
 class CallbackConfig(Config):
     @typechecked
     def __init__(self, data: Callback) -> None:
         super().__init__(data)
 
-    def get_config(self):
-        callback_attributes = self._data.__dict__
+    def get_config(self, data):
+        callback_attributes = data.__dict__
         public_attributes = {}
 
         for key,value in callback_attributes.items():
@@ -60,10 +93,10 @@ class CallbackConfig(Config):
             if key[0] != '_' and not isinstance(value, Callable):
                 public_attributes[key] = value
 
-        callback_name = self._data.__class__.__name__
+        callback_name = data.__class__.__name__
 
         config = {
             callback_name: public_attributes
         }
 
-        return 'callbacks', config
+        return ('callbacks', config)
