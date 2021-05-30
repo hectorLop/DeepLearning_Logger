aa22548c784e63a8ce75b8be25d7bab96fd1e501
hectorLop <lopez.almazan.hector@gmail.com>
2021-05-30 21:37:42 +0200

    Added PytorchLogger() class skeleton and modified .gitignore to ignore jupyter notebooks checkpoints

---
+++ deeplearning_logger/pytorch/pytorch_logger.py
@@ -0,0 +1,16 @@
+from typing import List
+import torch
+import torch.nn as nn
+
+from typing import List
+
+class PytorchLogger():
+    def __init__(self) -> None:
+        pass
+
+    def save(data: object):
+        pass
+
+class Parameters():
+    def __init__(self) -> None:
+        pass
